37819e1ae5e1189fadf0c098b6348e311a876f10
1mr1mar <ganbourmarwan9@gmail.com>
2025-06-19 22:59:41 +0100

    initialisation for the frontend and strat up in board component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Board from './components/Board/Board'
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Board />
+    </div>
+  )
+}
+
+export default App
